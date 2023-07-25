9caa010c89aa10cab3a4ff725584bbe47db22198
Miguel Manente <manente2011@gmail.com>
2023-07-25 15:26:10 -0300

    Practica en el DOM

---
+++ OneDrive/Escritorio/PracticaJS/UsoDocGetElemById/js/index.js
@@ -0,0 +1,9 @@
+function Accion(Tipo) {
+    var elem = document.getElementById('DivBotones');
+     if (Tipo == "Ocultar")
+        elem.style.display = 'none';
+     else 
+        elem.style.display = '';
+ }
+ 
+
