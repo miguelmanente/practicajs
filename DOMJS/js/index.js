ecca2ff502a8be02b613330c5258c9263f6be72d
Miguel <manente2011@gamil.com>
2025-01-16 16:46:56 -0300

    DOM en JS

---
+++ DOMJS/js/index.js
@@ -0,0 +1,32 @@
+/*const encabezadoH1 = document.getElementById("cabecera");
+    alert(
+      "Tipo del nodo: " +
+        encabezadoH1.nodeType +
+        "\n" + //Devuelve el tipo de nodo como un valor numérico en este caso devuelve 1 pq es un elemento del Html //
+        "Nombre del nodo: " +
+        encabezadoH1.nodeName + // Devuelve el nombre de la etiqueta (H1)
+        "\n" +
+        "Valor del nodo: " +  // Devuelve el contenido de un nodo de texto en este caso Null
+        encabezadoH1.nodeValue
+    );*/
+
+// Ver contenido de nodo asociado a la etiqueta H1
+// const encabezadoH1 = document.getElementById("cabecera"); /*Guardamos en una variable el atributo (iD) del elemento head*/
+// const textoEncabezadoH1 = encabezadoH1.firstChild;  /* Seleccionamos el 1er node con el atributo firstChild */
+// alert("El texto del encabezado <h1> es: "+ textoEncabezadoH1.nodeValue); /* Vemos el contendido con nodevalue */
+
+// o también se puede hacer de la siguiente manera
+//const encabezadoH1 = document.getElementById("cabecera");
+//alert("El texto del encabezado <h1> es: " + encabezadoH1.innerHTML);
+
+//Modificar estilos y atributos de los elementos HTML con style
+//const tamanioFuente = prompt("Tamaño de la fuente (en pixeles): ");
+//document.getElementById("cabecera").style.fontSize = tamanioFuente;
+
+
+
+
+
+
+
+
