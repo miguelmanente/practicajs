56ec0d3d5710782bd5f0ca2bfdd6377cc7f8b682
root <manente2011@gmail.com>
2025-03-18 20:14:08 -0300

    Mi servidor

--- MiServidor/server.js
+++ MiServidor/server.js
@@ -1,14 +1,36 @@
-//Creación del servidor Express
+import mysql from 'mysql2';
 
+//Creación del servidor Express
+ 
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express()
+app.use(express.json())  //lee JSON en el body
+
+//Configurar la conexion a MYSQL
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'Profesores'
+});
+
+//Conectar a MYSQL
+
+conexion.connect(err => {
+    if (err) {
+        console.log('Error conectando a MySQL:', err);
+        return;
+    }
+    console.log('Conectado a MySQL!!!')
+});
 
 //Definir el puerto
 
 const PORT = 3000;
 
 // Ruta principal
-
 app.get('/', (req, res) => {
     res.send('¡Hola mundo desde Express');
 });
